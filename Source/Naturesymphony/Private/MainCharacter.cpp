54c1f39db069fcf0754b2ac0fa9a79caaa34e0c4
Saygachenko <curatoroff@gmail.com>
2024-01-20 16:53:53 +0200

    Added a basic movement system for the main character.

---
+++ Source/Naturesymphony/Private/MainCharacter.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainCharacter.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (APlayerController* PlayerController = GetWorld()->GetFirstPlayerController())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* InputSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			if (InputMapping)
+			{
+				InputSystem->AddMappingContext(InputMapping, 0);
+			}
+		}
+	}
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* Input = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		Input->BindAction(MoveInputAction, ETriggerEvent::Triggered, this, &AMainCharacter::Move);
+		Input->BindAction(LookInputAction, ETriggerEvent::Triggered, this, &AMainCharacter::Look);
+		Input->BindAction(JumpInputAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+	}
+}
+
+void AMainCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D AxisValue = Value.Get<FVector2D>();
+
+	if (Controller && AxisValue.Y != 0.f || AxisValue.X != 0.f)
+	{
+		AddMovementInput(GetActorForwardVector(), AxisValue.Y);
+		AddMovementInput(GetActorRightVector(), AxisValue.X);
+	}
+}
+
+void AMainCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D AxisValue = Value.Get<FVector2D>();
+
+	AddControllerPitchInput(AxisValue.Y);
+	AddControllerYawInput(AxisValue.X);
+}
+
---
+++ Source/Naturesymphony/Public/MainCharacter.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+struct FInputActionValue;
+
+UCLASS()
+class NATURESYMPHONY_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* InputMapping;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* MoveInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* LookInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* JumpInputAction;
+
+private:
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+};
